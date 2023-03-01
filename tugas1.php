2529e4302bdaca1b074a8e7137a1fa7ccf6f29aa
Nur Rachmat <rachmat.nur91@gmail.com>
2023-03-01 15:06:38 +0700

    Soal Tugas1

---
+++ tugas1.php
@@ -0,0 +1,4 @@
+<?php 
+//Buatlah Associative Array dari DNS (Daftar Nilai Semester) kalian Tahun Akademik 2022 Ganjil
+//Tampilkan data dari Array DNS kalian menggunakan fungsi foreach
+//Tampilkan dalam Bentuk HTML Table
